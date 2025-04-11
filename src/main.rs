b38a124ea82d0cf0fe9f53ce9d998ff28226e6f5
someever <the.someever@gmail.com>
2025-04-11 05:57:03 +0300

    feat: add running tasks, simple output

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,23 @@
 //   run "npm run start" -d ./api
 
 use std::{
+    collections::HashMap,
     env::{self, Args},
+    io::{BufRead, BufReader, Read},
     iter::Peekable,
     path::PathBuf,
+    process::{Command, Stdio},
+    sync::mpsc,
 };
 
 #[derive(Debug)]
-struct Task {
+struct TaskDef {
     command: String,
     name: String,
     workdir: PathBuf,
 }
 
-fn parse_task(args: &mut Peekable<Args>, task_count: i32) -> Task {
+fn parse_task(args: &mut Peekable<Args>, task_count: i32) -> TaskDef {
     if !args.next().is_some_and(|arg| arg == "run") {
         panic!("expected run");
     }
@@ -34,7 +38,7 @@ fn parse_task(args: &mut Peekable<Args>, task_count: i32) -> Task {
         }
     }
 
-    Task {
+    TaskDef {
         name: name.or_else(|| workdir.clone()).unwrap_or(format!(
             "#{}: {}",
             task_count + 1,
@@ -47,6 +51,16 @@ fn parse_task(args: &mut Peekable<Args>, task_count: i32) -> Task {
     }
 }
 
+enum TaskMessage {
+    Stdout { task: usize, line: String },
+    Exited { task: usize },
+}
+
+struct Task {
+    id: usize,
+    name: String,
+}
+
 fn main() {
     let mut args = std::env::args().peekable();
     args.next();
@@ -56,5 +70,66 @@ fn main() {
         tasks.push(parse_task(&mut args, tasks.len() as i32));
     }
 
-    dbg!(tasks);
+    let (tx, rx) = mpsc::channel::<TaskMessage>();
+
+    let mut running_tasks = HashMap::new();
+    for (id, task) in tasks.into_iter().enumerate() {
+        let sender = tx.clone();
+        std::thread::spawn(move || {
+            let mut process = if cfg!(windows) {
+                Command::new("cmd.exe")
+                    .args(["/C", &task.command])
+                    .stdout(Stdio::piped())
+                    .spawn()
+            } else {
+                Command::new("sh")
+                    .args(["-c", &task.command])
+                    .stdout(Stdio::piped())
+                    .spawn()
+            }
+            .unwrap();
+
+            let stdout = process.stdout.take().unwrap();
+            let mut reader = BufReader::new(stdout);
+            let mut line = String::new();
+
+            loop {
+                let size = reader
+                    .read_line(&mut line)
+                    .expect("failed to read task output");
+                if size == 0 {
+                    let _ = sender.send(TaskMessage::Exited { task: id });
+                    break;
+                }
+                let _ = sender.send(TaskMessage::Stdout {
+                    task: id,
+                    line: line.clone(),
+                });
+            }
+        });
+        running_tasks.insert(
+            id,
+            Task {
+                name: task.name,
+                id,
+            },
+        );
+    }
+
+    for msg in rx {
+        match msg {
+            TaskMessage::Stdout { task, line } => {
+                let name = &running_tasks.get(&task).unwrap().name;
+                print!("{name}: {line}");
+            }
+            TaskMessage::Exited { task } => {
+                let name = &running_tasks.get(&task).unwrap().name;
+                println!("{name}: exited");
+                running_tasks.remove(&task);
+                if running_tasks.is_empty() {
+                    break;
+                }
+            }
+        }
+    }
 }
