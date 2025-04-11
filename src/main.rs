d79b094a539682020d266e0af753f4a00fec2b0f
someever <the.someever@gmail.com>
2025-04-11 17:57:14 +0300

    feat: basic output grouping

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@
 use std::{
     collections::HashMap,
     env::{self, Args},
-    io::{BufRead, BufReader, Read},
+    io::{stdout, BufRead, BufReader, Read, Stdout, Write},
     iter::Peekable,
     path::PathBuf,
     process::{Command, ExitStatus, Stdio},
     sync::mpsc,
 };
+
+use crossterm::{cursor, style, terminal, QueueableCommand};
 
 #[derive(Debug)]
 struct TaskDef {
@@ -58,7 +60,9 @@ enum TaskMessage {
 
 struct Task {
     id: usize,
+    completed: bool,
     name: String,
+    logs: Vec<String>,
 }
 
 fn main() {
@@ -72,7 +76,7 @@ fn main() {
 
     let (tx, rx) = mpsc::channel::<TaskMessage>();
 
-    let mut running_tasks = HashMap::new();
+    let mut running_tasks = Vec::new();
     for (id, task) in tasks.into_iter().enumerate() {
         let sender = tx.clone();
         std::thread::spawn(move || {
@@ -94,6 +98,7 @@ fn main() {
             let mut line = String::new();
 
             loop {
+                line.clear();
                 let size = reader
                     .read_line(&mut line)
                     .expect("failed to read task output");
@@ -108,26 +113,65 @@ fn main() {
                 });
             }
         });
-        running_tasks.insert(
+        running_tasks.push(Task {
+            name: task.name,
             id,
-            Task {
-                name: task.name,
-                id,
-            },
-        );
+            logs: Vec::new(),
+            completed: false,
+        });
     }
+
+    if running_tasks.len() == 0 {
+        panic!("no tasks specified");
+    }
+
+    let mut stdout = stdout();
+
+    for task in &running_tasks {
+        stdout
+            .queue(style::Print(format!("{}\n", task.name)))
+            .unwrap();
+    }
+    stdout.flush().unwrap();
+
+    let mut completed_count = 0;
 
     for msg in rx {
         match msg {
-            TaskMessage::Stdout { task, line } => {
-                let name = &running_tasks.get(&task).unwrap().name;
-                print!("{name}: {line}");
+            TaskMessage::Stdout { task: id, line } => {
+                let task = running_tasks.get_mut(id).unwrap();
+                task.logs.push(line.clone());
+
+                let mut distance = 0;
+                for task in &running_tasks[id + 1..] {
+                    distance += task.logs.len() + 1;
+                }
+
+                if distance > 0 {
+                    stdout.queue(cursor::MoveUp(distance as u16)).unwrap();
+                }
+                stdout
+                    .queue(terminal::Clear(terminal::ClearType::CurrentLine))
+                    .unwrap();
+                stdout.queue(style::Print(line)).unwrap();
+
+                for task in &running_tasks[id + 1..] {
+                    stdout
+                        .queue(style::Print(format!("{}\n", task.name)))
+                        .unwrap();
+                    for log in &task.logs {
+                        stdout.queue(style::Print(log)).unwrap();
+                    }
+                }
+
+                stdout.flush().unwrap();
             }
             TaskMessage::Exited { task, status } => {
-                let name = &running_tasks.get(&task).unwrap().name;
-                println!("{name}: exited {status:#?}");
-                running_tasks.remove(&task);
-                if running_tasks.is_empty() {
+                let task = running_tasks.get_mut(task).unwrap();
+                //println!("{}: exited {status:#?}", task.name);
+                task.completed = true;
+                completed_count += 1;
+                if completed_count == running_tasks.len() {
                     break;
                 }
             }
